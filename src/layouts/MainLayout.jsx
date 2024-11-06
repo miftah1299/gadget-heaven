ad8020be2dffff15c28b8f0f15b040e1804d9afe
miftah1299 <miftahjannat25@gmail.com>
2024-11-07 02:54:19 +0600

    add react-hot-toast for notifications and enhance layout with Navbar and Footer components

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,27 @@
 // Desc: Main Layout for the application
 
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import { Toaster } from "react-hot-toast";
+
 const MainLayout = () => {
     return (
         <div>
-            <h1>
-                Welcome to the Main Layout!
-            </h1>
+            <Toaster />
+            {/* Navbar */}
+            <div>
+                <Navbar />
+            </div>
+
+            {/* Main Section */}
+            <div>
+                {/* Dynamic content */}
+                <Outlet />
+            </div>
+
+            {/* Footer */}
+            <Footer />
         </div>
     );
 };
