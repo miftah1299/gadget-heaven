176f2aba7ee34a08059415b8fd2f9d63d54e1364
miftah1299 <miftahjannat25@gmail.com>
2024-11-07 04:45:48 +0600

    enhance Footer with navigation sections, update Heading to accept props

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,37 @@
+import Heading from "./Heading";
 
 const Footer = () => {
     return (
-        <div>
-            <h1>
-                Footer
-            </h1>
-        </div>
+        <footer className="footer max-w-screen-xl mx-auto flex flex-col items-center py-24">
+            <div>
+                <Heading
+                    title="Gadget Heaven"
+                    subtitle="Leading the way in cutting-edge technology and innovation."
+                />
+            </div>
+            <hr className="w-full" />
+            <div className="flex gap-40 text-center text-black/50">
+                <nav className="flex flex-col space-y-2">
+                    <h6 className="text-lg font-bold text-black">Services</h6>
+                    <a className="link link-hover">Product Support</a>
+                    <a className="link link-hover">Order Tracking</a>
+                    <a className="link link-hover">Shipping & Delivery</a>
+                    <a className="link link-hover">Returns</a>
+                </nav>
+                <nav className="flex flex-col space-y-2">
+                    <h6 className="text-lg font-bold text-black">Company</h6>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Careers</a>
+                    <a className="link link-hover">Contact</a>
+                </nav>
+                <nav className="flex flex-col space-y-2">
+                    <h6 className="text-lg font-bold text-black">Legal</h6>
+                    <a className="link link-hover">Terms of Service</a>
+                    <a className="link link-hover">Privacy Policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+            </div>
+        </footer>
     );
 };
 
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,10 +1,17 @@
+import PropTypes from "prop-types";
 
-const Heading = () => {
+const Heading = ({ title, subtitle }) => {
     return (
-        <div>
-            
+        <div className="flex flex-col gap-4 text-center my-12">
+            <h1 className="text-2xl lg:text-3xl">{title}</h1>
+            <p className="text-xs md:text-base text-black/50">{subtitle}</p>
         </div>
     );
+};
+
+Heading.propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string,
 };
 
 export default Heading;
