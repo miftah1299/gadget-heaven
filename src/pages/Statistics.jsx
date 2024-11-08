12cdbcc8bb784aed0cd66084e399105936e80d4f
miftah1299 <jannat27120@gmail.com>
2024-11-08 21:27:40 +0600

    refactor Statistics page to use Heading component and enhance layout

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,10 +1,15 @@
+import Heading from "../components/Heading";
 
 const Statistics = () => {
     return (
         <div>
-            <h1>
-                Welcome to the Statistics Page!
-            </h1>
+            <div className="bg-primary">
+                <Heading
+                    title="Statistics"
+                    subtitleClassName="w-1/2 mx-auto"
+                    subtitle="Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!"
+                ></Heading>
+            </div>
         </div>
     );
 };
